d5905d3e92636cbe3fdc9072df6118d1d7b65a74
eromose-le <erons24@yahoo.com>
2024-07-26 00:14:24 +0100

    chore: modify site manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,15 +6,25 @@ export default function manifest(): MetadataRoute.Manifest {
     name: appTitle,
     short_name: appTitle,
     description: appDescription,
-    start_url: "/",
+    start_url: "/app",
     display: "standalone",
     background_color: "#fcfbfc",
     theme_color: "#fcfbfc",
     icons: [
       {
         src: "/favicon.ico",
-        sizes: "any",
+        sizes: "16x16",
         type: "image/x-icon",
+      },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
       { src: "/sportgalaxy-192x192.png", sizes: "192x192", type: "image/png" },
       { src: "/sportgalaxy-512x512.png", sizes: "512x512", type: "image/png" },
