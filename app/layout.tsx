7badd90b1a13df3176a26a07bcdb8a85856c9416
eromose-le <erons.a.gberaese@gmail.com>
2025-11-03 11:29:33 +0100

    feat: add meta script

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,39 @@ export default function RootLayout({
       <body className={`${jost.className}`}>
         <ReactQueryProvider>
           <Script
+            id="meta-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document, 'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '2158765044666889');
+      fbq('track', 'PageView');
+    `,
+            }}
+          />
+
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+      <img
+        height="1"
+        width="1"
+        style="display:none"
+        src="https://www.facebook.com/tr?id=2158765044666889&ev=PageView&noscript=1"
+        alt="facebook-pixel"
+      />
+    `,
+            }}
+          />
+
+          <Script
             strategy="afterInteractive"
             src="https://www.googletagmanager.com/gtag/js?id=G-Y0F6H3CQZL"
           ></Script>
